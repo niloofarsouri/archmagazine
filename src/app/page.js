0d6e76ee5b6aa01447246ffc1e67caff14b74907
niloofar souri <nlfrsouri@gmail.com>
2025-06-09 15:13:50 +0330

    edit margin

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <section className="w-full min-h-screen">
 
-        <div className="w-full min-h-screen mb-50 md:h-[750px]">
+        <div className="w-full h-[2000px] bg-[#220C08] md:h-[750px]">
           <div className="w-full h-[400px] md:max-h-screen bg-[url(/backgroundSky.jpg)] bg-no-repeat bg-cover relative mb-60 flex justify-center items-center text-center ">
             <div className="w-full absolute top-0"><RootHeader /></div>
 
@@ -30,14 +30,14 @@ export default function Home() {
 
         <div className="w-full min-h-screen bg-[#220C08] flex flex-col justify-center items-center">
 
-          <div className="w-3/5 h-300px p-8 flex-col justify-center items-center text-white ">
+          <div className="w-3/5 h-auto mt-[300px] flex-1/2 p-8 flex-col justify-center items-center text-white ">
             <h1 className="text-center font-extrabold md:text-2xl">هنر یا علم ساختمان سازی</h1>
             <p className="p-4 text-xs text-justify font-extralight md:text-xl">
               این هنر است که معماری را از مهارت های مرتبط با ساخت متمایز می کند. معماری همان فرآیند ومحصول طراحی، تصور، برنامه ریزی، طراحی و ساخت ساختمان ها یا سازه ها می باشد. این اصطلاح از لاتین architectura از یونان باستان ἀρχιτέκτων (arkhitéktōn) 'معمار'; از ἀρχι- (arkhi-) 'رئیس'، و τέκτων (téktōn) 'خالق'. آثار معماری، در شکل مادی ساختمان ها، اغلب به عنوان نمادهای فرهنگی و به عنوان آثار هنری تلقی می شوند. تمدن های تاریخی اغلب با دستاوردهای معماری بازمانده از خود شناسایی می شوند.
             </p>
           </div>
 
-          <div className="w-full flex flex-col items-center mt-3 md:m-auto">
+          <div className="w-full h-auto flex-1/2 flex flex-col items-center mt-3 md:m-auto">
             <FirstAnimation />
           </div>
         </div>
