3edebe4fdf8351f5eab7f745e61b11cf7e72ac6b
niloofar souri <nlfrsouri@gmail.com>
2025-06-09 15:46:35 +0330

    edit in margin

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
 
             <div className="absolute top-48 md:top-31">
               <p className="text-white lg:text-[210px] xl:text-[250px] opacity-55 text-center font-primary lg:top-40 xl:top-33 hidden lg:block">ARCH MAG</p>
-              <p className="text-white text-[150px] md:text-[260px] opacity-55 text-center font-primary lg:hidden">ARCH</p>
+              <p className="text-white text-[150px] md:text-[340px] opacity-55 text-center font-primary lg:hidden">ARCH</p>
             </div>
 
             <div className="w-full min-h-screen bg-[url(/images/bg-2.png)] bg-no-repeat bg-cover absolute top-50 z-50"></div>
@@ -29,7 +29,7 @@ export default function Home() {
 
         <div className="w-full min-h-screen bg-[#220C08] flex flex-col justify-center items-center">
 
-          <div className="w-3/5 h-auto md:mt-[200px] flex-1/2 p-8 flex-col justify-center items-center text-white ">
+          <div className="w-3/5 h-auto mt-[100px] md:mt-[200px] flex-1/2 p-8 flex-col justify-center items-center text-white ">
             <h1 className="text-center font-extrabold md:text-2xl">هنر یا علم ساختمان سازی</h1>
             <p className="p-4 text-xs text-justify font-extralight md:text-xl">
               این هنر است که معماری را از مهارت های مرتبط با ساخت متمایز می کند. معماری همان فرآیند ومحصول طراحی، تصور، برنامه ریزی، طراحی و ساخت ساختمان ها یا سازه ها می باشد. این اصطلاح از لاتین architectura از یونان باستان ἀρχιτέκτων (arkhitéktōn) 'معمار'; از ἀρχι- (arkhi-) 'رئیس'، و τέκτων (téktōn) 'خالق'. آثار معماری، در شکل مادی ساختمان ها، اغلب به عنوان نمادهای فرهنگی و به عنوان آثار هنری تلقی می شوند. تمدن های تاریخی اغلب با دستاوردهای معماری بازمانده از خود شناسایی می شوند.
